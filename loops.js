dcaf5b8f76bced5b112d42329885eeae8db35816
nishanthmaraboina <nishanthmaraboina@gmail.com>
2018-11-09 19:30:55 +0530

    loops

--- loops.js
+++ loops.js
@@ -1,4 +1,29 @@
-//Types of For Loops
+// //Types of For Loops
 
-
-console.log("hai");
+// //For 1
+// for(var i=1 ; i <=10 ; i++){
+//   //  console.log(i);
+// }
+// //For 2
+// var i = 1;
+// for(;i<=10;i++){
+//     //console.log(i);
+// }
+// //For 3
+// var i = 1;
+// for(;i<=10;){
+//     //console.log(i);
+//     //i=i+1;
+// }
+// //For 4
+var i=1;
+for(;;)
+{
+    if(i<=10){
+       console.log(i);
+       i++;
+       continue;
+    }
+   if(i >10)
+    break;  
+}
