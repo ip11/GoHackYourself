4c18fdcce042130b40f304d10afe416f5eb67fca
nishanthmaraboina <nishanthmaraboina@gmail.com>
2018-11-12 14:32:58 +0530

    whileloops

--- loops.js
+++ loops.js
@@ -1,29 +1,64 @@
-// //Types of For Loops
+// // //Types of For Loops
 
-// //For 1
-// for(var i=1 ; i <=10 ; i++){
-//   //  console.log(i);
+// // //For 1
+// // for(var i=1 ; i <=10 ; i++){
+// //   //  console.log(i);
+// // }
+// // //For 2
+// // var i = 1;
+// // for(;i<=10;i++){
+// //     //console.log(i);
+// // }
+// // //For 3
+// // var i = 1;
+// // for(;i<=10;){
+// //     //console.log(i);
+// //     //i=i+1;
+// // }
+// // //For 4
+// var i=1;
+// for(;;)
+// {
+//     if(i<=10){
+//        console.log(i);
+//        i++;
+//        continue;
+//     }
+//    if(i >10)
+//     break;  
 // }
-// //For 2
-// var i = 1;
-// for(;i<=10;i++){
-//     //console.log(i);
+
+// for(var i=0; i<=5;i++){
+//     if(i==4){
+//         continue;
+//         //break;
+//     }
+//     console.log(i);    
 // }
-// //For 3
-// var i = 1;
-// for(;i<=10;){
-//     //console.log(i);
-//     //i=i+1;
+// var i=1;
+// while(i<=100){
+//     console.log(i);
+//     i++;
 // }
-// //For 4
-var i=1;
-for(;;)
-{
-    if(i<=10){
-       console.log(i);
-       i++;
-       continue;
-    }
-   if(i >10)
-    break;  
+// var k;
+// var i=1;
+// while(1){
+//     console.log(i);
+//     i++;
+//     if(i==Infinity)
+//         break
+// }
+do{
+    var k=0;
+    console.log(k);
 }
+while(k==5);
+
+
+
+
+
+{
+console.log("H")
+}
+
